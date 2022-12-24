cd7c2a9da6299ea63b747a356e02e3a747290149
Jay <jay63837@gmail.com>
2022-12-24 09:18:30 +0530

    Added jwt create, verify logic and compare the incoming password

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-
+const UserService = require('./services/user-service');
 const app = express();
 
 
@@ -17,6 +17,12 @@ const prepareAndStartServer = () => {
     app.use('/api',apiRoutes);
     app.listen(PORT, async () =>{
         console.log(`Server started ${PORT}`);
+        const service = new UserService();
+        // const newToken = service.createToken({email:'sanket@admin.com', id:1})
+        // console.log('New Token is :',newToken);
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjcxODUyNjgyLCJleHAiOjE2NzE4NTYyODJ9.WhzxMUZDE9mLq2uJPZevXjYlzuXIT1n3Mqm0HWxTEwc'
+        const response = service.verifyToken(token);
+        console.log(response);
     })
 
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const { JWT_KEY } = require('../config/serverConfig');
+
 const UserRepository = require('../repository/user-repository');
 
 class UserService{
@@ -9,6 +12,35 @@ class UserService{
         try {
             const user = await this.userRepository.create(data);
             return user;
+        } catch (error) {
+            console.log("Something went wrong on service layer");
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try {
+            const result = jwt.sign(user, JWT_KEY,{expiresIn: '1h'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong on service layer");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong on service layer");
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
         } catch (error) {
             console.log("Something went wrong on service layer");
             throw error;
